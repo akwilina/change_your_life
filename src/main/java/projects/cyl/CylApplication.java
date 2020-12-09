99a9a96a58913c03125a847a062a58ed1de9f059
delola\delola lokal <aleksandra.man@gmail.com>
2020-12-09 18:55:33 +0100

    structure :: initial project structure

---
+++ src/main/java/projects/cyl/CylApplication.java
@@ -0,0 +1,13 @@
+package projects.cyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CylApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CylApplication.class, args);
+	}
+
+}
